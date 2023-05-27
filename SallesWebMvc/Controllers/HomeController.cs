47f7ae0042ce93fd06c6791aaa8f9ce4cf6cda91
rafaeg <rafaeg.rj22@hotmail.com>
2023-05-27 19:43:39 -0300

    Modificação do controller HomeController

--- SallesWebMvc/Controllers/HomeController.cs
+++ SallesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace SallesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Rafael";
 
             return View();
         }
